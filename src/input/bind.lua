eb02c446f4912e5c7bfea9d4b56daf155e83e184
airstruck <airstruck>
2016-08-03 15:31:50 -0400

    Use sequence table for binds

--- src/input/bind.lua
+++ src/input/bind.lua
@@ -10,19 +10,29 @@ local bind = {
 ]]
 
 function bind:addBind(identifier, key, func)
-    self.binds[identifier] = {key, func};
+    self.binds[#self.binds + 1] = {identifier, key, func};
 end
 
 function bind:removeBind(identifier)
-	self.binds[identifier] = nil;
+	for i = #self.binds, 1, -1 do
+	    if self.binds[i].identifier == identifier then
+	        table.remove(self.binds, i)
+	        return
+	    end
+	end
 end
 
 function bind:addMouseBind(identifier, button, func)
-    self.mouseBinds[identifier] = {key, func}
+    self.mouseBinds[#self.mouseBinds + 1] = {identifier, button, func};
 end
 
 function bind:removeMouseBind(identifier)
-	self.mouseBinds[identifier] = nil;
+	for i = #self.mouseBinds, 1, -1 do
+	    if self.mouseBinds[i].identifier == identifier then
+	        table.remove(self.mouseBinds, i)
+	        return
+	    end
+	end
 end
 
 function bind:update(dt)
@@ -30,35 +40,36 @@ function bind:update(dt)
 end
 
 function bind:keyreleased(key)
-    for k,v in pairs(self.binds) do
-        if v[1] == key then
-            v[2](false);
+    for k,v in ipairs(self.binds) do
+        if v[2] == key then
+            v[3](false);
         end
     end
 end
 
 function bind:keypressed(key, isrepeat)
-    for k,v in pairs(self.binds) do
-        if v[1] == key then
-            v[2](true, isrepeat);
+    for k,v in ipairs(self.binds) do
+        if v[2] == key then
+            v[3](true, isrepeat);
         end
     end
 end
 
 function bind:mousepressed(x, y, button, touch)
-    for k,v in pairs(self.mouseBinds) do
-        if v[1] == key then
-            v[2](true, x, y, button, touch);
+    for k,v in ipairs(self.mouseBinds) do
+        if v[2] == button then
+            v[3](true, x, y, button, touch);
         end
     end
 end
 
 function bind:mousereleased(x, y, button, touch)
-    for k,v in pairs(self.mouseBinds) do
-        if v[1] == key then
-            v[2](false, x, y, button, touch);
+    for k,v in ipairs(self.mouseBinds) do
+        if v[2] == button then
+            v[3](false, x, y, button, touch);
         end
     end
 end
 
 return bind;
+
