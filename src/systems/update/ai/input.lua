cbb092f2f928ddeee1eb468c42b0b9f724deb973
Wyatt Marks <inbox@wyattmarks.com>
2016-12-31 17:19:45 -0600

    Made enemies chase you when they can see you

--- src/systems/update/ai/input.lua
+++ src/systems/update/ai/input.lua
@@ -4,9 +4,20 @@ return function (e, dt)
 	e.timer = (e.timer or 0) - dt
 
 	if e.timer <= 0 then
-		e.timer = math.random(1,4)
-        e.xvel = math.random() * 2 * e.speed - e.speed
-        e.yvel = math.random() * 2 * e.speed - e.speed
+		if e.targetX and e.targetY then
+			e.timer = math.random() * 2
+			local x, y = e.targetX, e.targetY
+			local pX = e.x + ((e.width / 2) or 0)
+			local pY = e.y + ((e.height / 2) or 0)
+			local angle = math.atan2(x - pX, y - pY)
+			local speed = math.random() * 2 * e.speed
+			e.xvel = speed * math.sin(angle)
+			e.yvel = speed * math.cos(angle)
+		else
+			e.timer = math.random(1,4)
+			e.xvel = math.random() * 2 * e.speed - e.speed
+			e.yvel = math.random() * 2 * e.speed - e.speed
+		end
 	end
 end
 
